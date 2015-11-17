2a96cd369d2f8fa194f8eb3aa35c01fca8d1b117
menooker <myjisgreat@live.cn>
2015-11-17 10:26:42 +0800

    2

--- src/freesqlbase/FreeSQLBase.java
+++ src/freesqlbase/FreeSQLBase.java
@@ -43,7 +43,7 @@ public class FreeSQLBase {
 			try {
 				Connection con = null; // 定义一个MYSQL链接对象
 				Class.forName("com.mysql.jdbc.Driver").newInstance(); // MYSQL驱动
-				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test", "root", "thisissql"); // 链接本地MYSQL
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/freebase", "root", "thisissql"); // 链接本地MYSQL
 				System.out.print("yes");
 			} catch (Exception e) {
 				System.out.println("MYSQL ERROR:" + e.getMessage());
