243dcad243a3f026e3dc45f13c04d70ccfe89c13
menooker <myjisgreat@live.cn>
2015-11-21 16:20:59 +0800

    pool

--- src/freesqlbase/FreeSQLBase.java
+++ src/freesqlbase/FreeSQLBase.java
@@ -2,6 +2,9 @@ package freesqlbase;
 
 import java.io.*;
 import java.nio.ByteBuffer;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.zip.GZIPInputStream;
 
 
@@ -14,11 +17,25 @@ import java.sql.Statement;
 public class FreeSQLBase {
 	static PipedOutputStream pos;
 	static PipedInputStream pis;
-
+	static Connection con = null; // 定义一个MYSQL链接对象
+	static ExecutorService pool = Executors.newFixedThreadPool(4);
 	
 	static void parse(String[] line)
 	{
 		System.out.printf("%s,%s,%s\n",line[0],line[1],line[2]);
+	}
+	
+	static private final class StringTask implements Callable<String> {
+		public String[] lines;
+		public StringTask(String[] p)
+		{
+			lines=p;
+		}
+		public String call() {
+			// Long operations
+			parse(lines);
+			return null;
+		}
 	}
 	
 	static Thread readth = new Thread() {
@@ -34,7 +51,8 @@ public class FreeSQLBase {
 				try {
 					line = reader.readLine();
 					String[] sp=line.split("\t");
-					parse(sp);
+					StringTask tsk=new StringTask(sp);
+					pool.submit(tsk);
 					
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -54,12 +72,12 @@ public class FreeSQLBase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			/*try {
-				Connection con = null; // 定义一个MYSQL链接对象
+			try {
+				
 				Class.forName("com.mysql.jdbc.Driver").newInstance(); // MYSQL驱动
-				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/freebase", "root", "thisissql"); // 链接本地MYSQL
+				con = DriverManager.getConnection("jdbc:mysql://202.120.37.25:23334/master", "root", "thisismysql"); // 链接本地MYSQL
 				System.out.println("yes");
-				Statement stmt;
+				/*Statement stmt;
 				stmt = con.createStatement();
 				stmt.executeUpdate("INSERT INTO test VALUES (1,'KK')");
 				ResultSet res = stmt.executeQuery("select * from test");
@@ -69,12 +87,11 @@ public class FreeSQLBase {
 					ret_id = res.getInt(1);
 					name = res.getString(2);
 					System.out.println(ret_id+" "+name);
-				}
+				}*/
 			} catch (Exception e) {
 				System.out.println("MYSQL ERROR:" + e.getMessage());
-			}*/
+			} 
 
-			
 			pos = new PipedOutputStream(); pis = new PipedInputStream(pos);
 			FileInputStream s = new FileInputStream(
 					new File("/media/NEWSMY/freebase.gz")); 
