a9ba4d29ebc2bdc5838c343c5b4c6eeb797f1df7
Menooker <myjisgreat@live.cn>
2015-11-23 16:20:22 +0800

    memory limit

--- go.sh
+++ go.sh
@@ -1,1 +1,1 @@
-java  -classpath ./mysql-connector-java-5.1.22-bin.jar:./bin freesqlbase.FreeSQLBase
+java -Xms128m -Xmx4096m -XX:-UseGCOverheadLimit -classpath ./mysql-connector-java-5.1.22-bin.jar:./bin freesqlbase.FreeSQLBase
